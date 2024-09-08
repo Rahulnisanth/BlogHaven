95d5510290074b96614e40bc1317f723a5b47506
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-09-08 19:00:05 +0530

    altered single post modal

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -19,15 +19,17 @@ const Modal: React.FC<ModalProps> = ({ details }) => {
         <div className="flex min-h-full items-center justify-center p-4">
           <DialogPanel
             transition
-            className="w-full max-w-5xl rounded-xl bg-white/5 p-10 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+            className="w-full max-w-5xl rounded-xl bg-white/5 p-5 md:p-10 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
           >
             <DialogTitle
               as="h3"
-              className="text-3xl font-extrabold text-teal-500"
+              className="text-2xl md:text-3xl font-extrabold text-teal-500"
             >
               {title}
             </DialogTitle>
-            <p className="my-6 text-justify text-md text-white">{content}</p>
+            <p className="my-4 md:my-6 text-justify text-md text-white">
+              {content}
+            </p>
           </DialogPanel>
         </div>
       </div>
